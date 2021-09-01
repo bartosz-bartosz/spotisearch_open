69062cfe3c48691c5ff23aca74980e6624508cb9
bartosz <badalecki@gmail.com>
2021-09-02 01:54:34 +0200

    Exporting all CSV's at once is possible, but bugged

--- spotisearch.py
+++ spotisearch.py
@@ -77,118 +77,128 @@ class Program:
 		    print("\nLast 50 saved tracks:\n\n")
 		    print(idx, track['artists'][0]['name'], " – ", track['name'])
 
-
+	'''Creates playlist instances and prints them'''
 	def create_playlist_instances(self):
-		playlisty = self.sp.user_playlists(self.username, limit=75, offset=1)
-
-		for idx, item in enumerate(playlisty['items']):
-			playlist_index = idx
-			playlist_id = item['id']
-			playlist_name = item['name']
-			playlist_tracklist = {}
-			print(f'PLAYLISTA: {playlist_name}, ID: {playlist_id}')
-
-			playlist_tracks = self.sp.playlist_tracks(playlist_id)
-			for track_idx, item in enumerate(playlist_tracks['items']):
-				track_data = item['track']
-				track_name = track_data['name']
-				track_id = track_data['id']
-				
-				for artist in track_data['artists']:
-					artist_id = artist['id']
-					artist_name = artist['name']
-
-				playlist_tracklist[track_idx] = [track_id, track_name, artist_name]
-
-			self.playlist_list.append(Playlist(playlist_index, playlist_id, playlist_name, playlist_tracklist))
-
-		for x in self.playlist_list:
-			print(f'''{x.playlist_index}: {x.playlist_name}\n''')
-			for k, v in x.playlist_tracklist.items():
-				print(f'{k} --- {v[2]} --- by {v[1]} ')
-			print('\n\n')
-
-	'''Not working yet'''
-	def create_multiple_csv(self):
-		needed_keys = ['name', 'artist', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'id']
-
-		for i in self.playlist_list:
-			csv_lists = []
-			csv_lists.append(needed_keys)
-			for k, v in i.playlist_tracklist.items():
-				pass
-
-		
-	'''This one tries to overcome limit limitations'''
-	def get_playlists(self): 
 		results = self.sp.user_playlists(self.username, offset=1)
-		playlists = results['items']
-		print(results)
+		playlisty = results['items']
 		indexy = []
 		while results['next']:
 			results = self.sp.next(results)
-			playlists.extend(results['items'])
-			for idx, item in enumerate(playlists):
+			playlisty.extend(results['items'])
+			for idx, item in enumerate(playlisty):
 				indexy.append(str(idx))
-				for k, v in item.items():
-					metki = ['name']
-					#print(k)
-					if k in metki:
-						print(f'{idx} - {v}')
-					else:
-						continue
-		
-		options = ['1', '2']
+				playlist_index = idx
+				playlist_id = item['id']
+				playlist_name = item['name']
+				playlist_tracklist = {}
+				print(f'PLAYLISTA: - {idx} - {playlist_name}, ID: {playlist_id}')
+
+				playlist_tracks = self.sp.playlist_tracks(playlist_id)
+				for track_idx, item in enumerate(playlist_tracks['items']):
+					track_data = item['track']
+					track_name = track_data['name']
+					track_id = track_data['id']
+					
+					for artist in track_data['artists']:
+						artist_id = artist['id']
+						artist_name = artist['name']
+
+					playlist_tracklist[track_idx] = [track_id, track_name, artist_name]
+
+				self.playlist_list.append(Playlist(playlist_index, playlist_id, playlist_name, playlist_tracklist))
+
+		options = ['1', '2', '3']
 		choice = input('''\n OPTIONS:\n
 			1 - Show playlist info\n
 			2 - Back to menu\n
-
+			3 - CSV for all playlists\n
 			Choose option:   ''')
 		if choice not in options:
 			return self.options_menu()
+		elif choice == '3':
+			return self.create_multiple_csv()
 		elif choice == '2':
 			return self.options_menu()
 		elif choice == '1':
-			return self.get_playlist_info(results, indexy)
+			return self.get_playlist_info(playlisty)
+
+	# def show_playlist(self):
+	# 	for x in self.playlist_list:
+	# 		print(f'''{x.playlist_index}: {x.playlist_name}\n''')
+	# 		for k, v in x.playlist_tracklist.items():
+	# 			print(f'{k} --- {v[2]} --- by {v[1]} ')
+	# 		print('\n\n')
+		
+	'''Probably not useful anymore'''
+	# def get_playlists(self): 
+	# 	results = self.sp.user_playlists(self.username, offset=1)
+	# 	playlists = results['items']
+	# 	print(results)
+	# 	indexy = []
+	# 	while results['next']:
+	# 		results = self.sp.next(results)
+	# 		playlists.extend(results['items'])
+	# 		for idx, item in enumerate(playlists):
+	# 			indexy.append(str(idx))
+	# 			for k, v in item.items():
+	# 				metki = ['name']
+	# 				#print(k)
+	# 				if k in metki:
+	# 					print(f'{idx} - {v}')
+	# 				else:
+	# 					continue
+		
+	# 	options = ['1', '2']
+	# 	choice = input('''\n OPTIONS:\n
+	# 		1 - Show playlist info\n
+	# 		2 - Back to menu\n
+
+	# 		Choose option:   ''')
+	# 	if choice not in options:
+	# 		return self.options_menu()
+	# 	elif choice == '2':
+	# 		return self.options_menu()
+	# 	elif choice == '1':
+	# 		return self.get_playlist_info(results, playlisty)
 
 	'''Get playlist info (especially playlist_id) and direct to fuction showing list of songs in playlist'''
-	def get_playlist_info(self, results, indexy): 
+	def get_playlist_info(self, playlisty): 
 		choice = input('\nChoose playlist number to show more info:  ')
-		playlisty = results['items']
-
-		if choice in indexy:
-			for idx, item in enumerate(playlisty):
-				if str(idx) == choice:
-					playlist_id = item['id']
-					print('\n')
-					# for k, v in item.items():  # Prints dictionary of playlist object (for dev use)
-					# 	print(f'{k} - {v}')
-			return self.show_playlist_tracks(playlist_id)	
-		else:
-			print('Index not found.\n')
-			return self.get_playlist_info(results, indexy)
+		for item in self.playlist_list:
+			print(type(item.playlist_index))
+			if item.playlist_index == int(choice):
+			# 	playlist_id = item['id']
+				print('\n')
+				# for k, v in item.items():  # Prints dictionary of playlist object (for dev use)
+				# 	print(f'{k} - {v}')
+				playlist_id = str(item.playlist_index)
+				return self.show_playlist_tracks(item)	
+			else:
+				print('Index not found.\n')
+				continue
 
 	'''Shows list of songs in given playlist (Artist name, song name, album name and album year release)'''
-	def show_playlist_tracks(self, playlist_id): 
-		playlist_tracks = self.sp.playlist_tracks(playlist_id)
+	def show_playlist_tracks(self, item, all_csv=False): 
+		print(item.playlist_name)
+		playlist_tracks = self.sp.playlist_tracks(item.playlist_id)
 		data_items = playlist_tracks['items']
-		print(data_items)
-
+		#print(data_items)
+		while playlist_tracks['next']:
+					playlist_tracks = self.sp.next(playlist_tracks)
+					data_items.extend(playlist_tracks['items'])
 		indexy = []
 		id_dict = {}
 		for idx, data_item in enumerate(data_items):
 			indexy.append(str(idx))
 			# Define variables for certain song info
 			track_data = data_item['track']
-
 			track_name = track_data['name']
 			track_id = track_data['id']
 			track_popularity = track_data['popularity']
 			track_album = track_data['album']
 			track_album_name = track_album['name']
 			track_album_release = track_album['release_date']
-			track_album_id = track_album['id']
-			
+			track_album_id = track_album['id']			
 			track_artists = track_data['artists']
 			for artist in track_artists:
 				artist_id = artist['id']
@@ -201,31 +211,36 @@ class Program:
 			print(f'''{idx}: {artist_name} - {track_name}\nFrom album "{track_album_name}", released on {track_album_release}\n''')
 		#print(id_dict)
 		#print(indexy)
-		input('Press ENTER... ')
-		'''Ask if user wants to get song info'''
-		options = ['1', '2', '3']
-		print('''\n 		--- OPTIONS ---\n
-			1 - Show song info\n
-			2 - Save playlist features to CSV file\n
-			Anything else - Go back to main menu\n''')
+		if all_csv==False:
+			input('Press ENTER... ')
+			'''Ask if user wants to get song info'''
+			options = ['1', '2', '3']
+			print('''\n 		--- OPTIONS ---\n
+				1 - Show song info\n
+				2 - Save playlist features to CSV file\n
+				Anything else - Go back to main menu\n''')
 
-		choice = input('Choose option:   ')
-		if choice == '1':
-			track_index = input("Choose song index: ")
-			if track_index in indexy:
-				chosen_track_id = id_dict[track_index]
-				return self.show_track_info(chosen_track_id)
-			else: 
-				print('Index not found.\n')
-				return self.show_playlist_tracks(playlist_id)
-		elif choice == '2':
+			choice = input('Choose option:   ')
+			if choice == '1':
+				track_index = input("Choose song index: ")
+				if track_index in indexy:
+					chosen_track_id = id_dict[track_index]
+					return self.show_track_info(chosen_track_id)
+				else: 
+					print('Index not found.\n')
+					return self.show_playlist_tracks(playlist_id)
+			elif choice == '2':
+				print("Please wait...")
+				print("Collecting data...")
+				return self.playlist_audio_features(id_dict)
+			else:
+				return self.options_menu()
+		else:
 			print("Please wait...")
 			print("Collecting data...")
-			return self.playlist_audio_features(id_dict)
-		else:
-			return self.options_menu()
+			return self.playlist_audio_features(id_dict, item, all_csv)
 
-	'''Will get song data and print some of it'''
+	'''Get song data and print some of it'''
 	def show_track_info(self, chosen_track_id):
 		chosen_track = self.sp.audio_features(chosen_track_id)
 		print(f'Your track ID is {chosen_track_id}')
@@ -235,7 +250,7 @@ class Program:
 		return self.options_menu()
 
 	'''Gets a list of features for all tracks in a playlists and makes it possible to save it in CSV file'''
-	def playlist_audio_features(self, id_dict):
+	def playlist_audio_features(self, id_dict, item, all_csv=False):
 		needed_keys = ['name', 'artist', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'id']
 		csv_lists = []
 		csv_lists.append(needed_keys)
@@ -252,26 +267,56 @@ class Program:
 			# print(track_features_list)
 			csv_lists.append(track_features_list)
 		#print(csv_lists)				
-
-		print('''\n 		--- OPTIONS ---\n
-			1 - Save playlist data to CSV\n
-			Anything else - Go back to main menu\n''')
-		choice = input('Choose option: ')
-		if choice == '1':
-			return self.create_playlist_csv(csv_lists)
+		if all_csv==False:
+			print('''\n 		--- OPTIONS ---\n
+				1 - Save playlist data to CSV\n
+				Anything else - Go back to main menu\n''')
+			choice = input('Choose option: ')
+			if choice == '1':
+				return self.create_playlist_csv(csv_lists)
+			else:
+				return self.options_menu()
 		else:
-			return self.options_menu()
+			return self.create_playlist_csv(csv_lists, item, all_csv=True)
 
-
-	def create_playlist_csv(self, csv_lists):
+	'''Save CSV file with a name'''
+	def create_playlist_csv(self, csv_lists, item, all_csv=False):
 		# print(csv_lists)
-		file_name = input('Type file name for CSV file: ')
+		if all_csv==False:
+			file_name = input('Type file name for CSV file: ')
+		else:
+			file_name = f"{item.playlist_name}"
 		with open(f'./analyzer/data/{file_name}.csv', 'w', encoding='utf8', newline='') as file:
 			writer = csv.writer(file)
 			writer.writerows(csv_lists)
 		print('Saving complete.\n')
-		input('Press KEY to go back to main menu.')
+		if all_csv==False:
+			input('Press KEY to go back to main menu.')
+			return self.options_menu()
+		else:
+			pass
+
+	'''Not working yet'''
+	def create_multiple_csv(self):
+		for item in self.playlist_list:
+			if os.path.isfile(f'./analyzer/data/{item.playlist_name}.csv'):
+				print(f"{item.playlist_name} already exported.")
+				continue
+			else:
+				self.show_playlist_tracks(item, all_csv=True)
+				print(f"{item.playlist_name}.csv saved.")
+		print('ALl playlists exported.\n')
 		return self.options_menu()
+
+
+
+		# needed_keys = ['name', 'artist', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'id']
+
+		# for i in self.playlist_list:
+		# 	csv_lists = []
+		# 	csv_lists.append(needed_keys)
+		# 	for k, v in i.playlist_tracklist.items():
+		# 		pass
 
 	'''Main options menu'''
 	def options_menu(self): # 
@@ -279,11 +324,11 @@ class Program:
 		os.system('cls')
 		options = ['1', '2', '3', '4', '5']
 		print('''\n 		--- OPTIONS ---\n
-			1 - Show account info\n
-			2 - Create playlist instances (testing)\n
-			3 - Show last user saved tracks\n
-			4 - Show playlists\n
-			5 - Exit\n''')
+	1 - Show account info\n
+	2 - Create playlist instances (testing)\n
+	3 - Show last user saved tracks\n
+	4 - Show playlists\n
+	5 - Exit\n''')
 		choice = input('Choose option:   ')
 		if choice not in options:
 			return self.options_menu()
@@ -314,7 +359,7 @@ class Program:
 		self.set_variables()
 		self.options_menu()
 
-
+'''----------------------------------------------------------------'''
 
 prg = Program(SCOPE)
 
